f241804fda94236f8313e81a128d4dd6606caa31
iagofelipe456-wq <iago.felipe456@gmail.com>
2026-08-22 22:05:59 -0300

    Create documentos.js

---
+++ documentos.js
@@ -0,0 +1,19 @@
+const documentos = [
+    {
+        titulo: "História de Parelhas",
+        ano: "1920",
+        texto: "Parelhas fazia parte da organização territorial do Seridó e estava relacionada ao município de Jardim do Seridó."
+    },
+
+    {
+        titulo: "Emancipação política de Parelhas",
+        ano: "1927",
+        texto: "O município de Parelhas foi criado por meio de sua emancipação política, passando a possuir administração municipal própria."
+    },
+
+    {
+        titulo: "Desenvolvimento urbano de Parelhas",
+        ano: "1950",
+        texto: "Durante o século XX, Parelhas apresentou crescimento urbano e expansão de suas atividades comerciais e administrativas."
+    }
+];
